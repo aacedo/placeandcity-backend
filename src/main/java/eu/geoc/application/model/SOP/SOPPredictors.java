b011c734f6fbfb1694bbf7e772da4491a0ab0f8c
aacedo <albert.acedo@gmail.com>
2017-04-07 15:33:35 +0100

    SOP predictors changed

--- src/main/java/eu/geoc/application/model/SOP/SOPPredictors.java
+++ src/main/java/eu/geoc/application/model/SOP/SOPPredictors.java
@@ -9,80 +9,51 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class SOPPredictors {
-    private Integer ex1;
-    private Integer ex2;
-    private Integer ex3;
-    private Integer ex4;
-    private Integer ex5;
-    private Integer ex6;
-    private Integer ex7;
+    private Integer na1;
+    private Integer na2;
+    private Integer na3;
+    private Integer na4;
 
     public SOPPredictors() {
     }
 
-    public SOPPredictors(Integer ex1, Integer ex2, Integer ex3, Integer ex4, Integer ex5, Integer ex6, Integer ex7) {
-        this.ex1 = ex1;
-        this.ex2 = ex2;
-        this.ex3 = ex3;
-        this.ex4 = ex4;
-        this.ex5 = ex5;
-        this.ex6 = ex6;
-        this.ex7 = ex7;
+    public SOPPredictors(Integer na1, Integer na2, Integer na3, Integer na4) {
+        this.na1 = na1;
+        this.na2 = na2;
+        this.na3 = na3;
+        this.na4 = na4;
     }
 
-    public Integer getEx1() {
-        return ex1;
+    public Integer getNa1() {
+        return na1;
     }
 
-    public void setEx1(Integer ex1) {
-        this.ex1 = ex1;
+    public void setNa1(Integer na1) {
+        this.na1 = na1;
     }
 
-    public Integer getEx2() {
-        return ex2;
+    public Integer getNa2() {
+        return na2;
     }
 
-    public void setEx2(Integer ex2) {
-        this.ex2 = ex2;
+    public void setNa2(Integer na2) {
+        this.na2 = na2;
     }
 
-    public Integer getEx3() {
-        return ex3;
+    public Integer getNa3() {
+        return na3;
     }
 
-    public void setEx3(Integer ex3) {
-        this.ex3 = ex3;
+    public void setNa3(Integer na3) {
+        this.na3 = na3;
     }
 
-    public Integer getEx4() {
-        return ex4;
+    public Integer getNa4() {
+        return na4;
     }
 
-    public void setEx4(Integer ex4) {
-        this.ex4 = ex4;
+    public void setNa4(Integer na4) {
+        this.na4 = na4;
     }
 
-    public Integer getEx5() {
-        return ex5;
-    }
-
-    public void setEx5(Integer ex5) {
-        this.ex5 = ex5;
-    }
-
-    public Integer getEx6() {
-        return ex6;
-    }
-
-    public void setEx6(Integer ex6) {
-        this.ex6 = ex6;
-    }
-
-    public Integer getEx7() {
-        return ex7;
-    }
-
-    public void setEx7(Integer ex7) {
-        this.ex7 = ex7;
-    }
 }
